b07f9d20e8a5ebbf709b94b02361964f67905f63
linuxgurugamer@gmail.com <linuxgurugamer@gmail.com>
2025-07-17 14:45:16 -0400

    Updated version file to match released version numbers
    Updated changelog

--- Source/AssemblyVersion.cs
+++ Source/AssemblyVersion.cs
@@ -5,5 +5,5 @@
   
  using System.Reflection;
 
- [assembly: AssemblyVersion("1.0.18.1")]
- [assembly: AssemblyFileVersion("1.0.18.1")]
+ [assembly: AssemblyVersion("18.1.0.0")]
+ [assembly: AssemblyFileVersion("18.1.0.0")]
